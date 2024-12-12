decfbe5ef58e75980d438eaf1bed1f56e5d802ca
Amélie Guyot <amelieguyot7@gmail.com>
2024-12-12 13:02:12 +0100

    feat(3-back-users): add Get Post Users

--- back/site-e-commerce/controllers/authController.js
+++ back/site-e-commerce/controllers/authController.js
@@ -1,1 +1,103 @@
-const express = require("express");
+const User = require("../models/User");
+const Address = require("../models/Address");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const registerUser = async (req, res) => {
+  const { firstName, lastName, email, password, address } = req.body;
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newAddress = await Address.create({
+      street: address.street,
+      city: address.city,
+      postal_code: address.postal_code,
+      country: address.country,
+      phone: address.phone,
+    });
+
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      address_id: newAddress._id,
+    });
+
+    res.status(201).json(user);
+  } catch (error) {
+    {
+      res.status(500).json({ error: error.message || "User creation failed" });
+    }
+  }
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(401).json({ error: "Authentification failed" });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (!match) {
+      return res.status(401).json({ error: "Authentification failed" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .json(user);
+  } catch (error) {
+    res.status(500).json({ error: error.message || "Authentification failed" });
+  }
+};
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message || "Error in recovery of users.",
+    });
+  }
+};
+
+/*maybe for recovery adress of user when He want to pay*/
+const getOneUser = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const userWithAddress = await User.findById(id).populate("address_id");
+
+    if (!userWithAddress) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(userWithAddress);
+  } catch (error) {
+    res.status(500).json({
+      error: error.message || "Error in recovery of the user",
+    });
+  }
+};
+
+module.exports = {
+  registerUser,
+  login,
+  getAllUsers,
+  getOneUser,
+};
--- back/site-e-commerce/routes/auth.js
+++ back/site-e-commerce/routes/auth.js
@@ -1,4 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const {
+  registerUser,
+  login,
+  getAllUsers,
+  getOneUser,
+} = require("../controllers/authController");
+const {
+  verifyToken,
+  validateRegister,
+  validateLogin,
+} = require("../middlewares/authMiddleware");
+
+router.post("/sign-up", validateRegister, registerUser);
+router.post("/login", validateLogin, login);
+router.get("/users", verifyToken, getAllUsers); //prof example but usefull in app ? maybe panel admin add middleware admin?
+router.get("/user/:id", verifyToken, getOneUser);
 
 module.exports = router;
