caf847d19cf1bdc6f003be29d903e8300ea1c407
Amélie Guyot <amelieguyot7@gmail.com>
2026-09-08 10:14:42 +0200

    modify index

--- back/site-e-commerce/routes/payments.js
+++ back/site-e-commerce/routes/payments.js
@@ -1,15 +1,9 @@
 const express = require("express");
-const router = express.Router();
-const {
-  createCheckoutSession,
-  stripeWebhook,
-} = require("../controllers/paymentsController");
 
-router.post(
-  "/api/stripe-webhook",
-  express.raw({ type: "application/json" }),
-  stripeWebhook,
-);
+const router = express.Router();
+
+const { createCheckoutSession } = require("../controllers/paymentsController");
+
 router.post("/create-checkout-session", createCheckoutSession);
 
 module.exports = router;
